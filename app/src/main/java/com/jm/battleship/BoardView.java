b4f68a9b8581e8d72a0470f5fea01df9d8e2842d
jm <phamquangthuy1997@gmail.com>
2020-11-02 21:24:02 +0900

    updated ship images

--- app/src/main/java/com/jm/battleship/BoardView.java
+++ app/src/main/java/com/jm/battleship/BoardView.java
@@ -7,9 +7,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
